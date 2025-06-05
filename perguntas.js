6f2a481e2b224f5cb07241bcd88d29e7d71a3b6f
harumi <hashiura.fonseca@escola.pr.gov.br>
2025-06-05 07:52:28 -0300

    05/06

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Dead by Daylight',
     'Qual é o objetivo dos sobreviventes?',
-    'Reparar geradores e escapar do mapa.'
+    'Reparar geradores e escapar do mapa'
 )
 
 criaCartao(
     'Dead by Daylight',
     'O que é a Escotilha (Hatch)?',
-    'Uma saída secreta que aparece quando resta apenas um sobrevivente.'
+    'Uma saída secreta que aparece quando resta apenas um sobrevivente'
 )
 
 criaCartao(
     'Língua Inglesa',
     'Qual é o passado do verbo “go”?',
-    'Went.'
+    'Went'
 )
 
 criaCartao(
     'Língua Inglesa',
     'Como se diz “amigo” em inglês?',
-    'Friend.'
+    'Friend'
 )
